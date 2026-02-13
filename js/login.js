148c9286accfd7f39c0aa599cb52fb9296735be4
TeYoMaRu <tonteuy55@gmail.com>
2026-02-13 13:55:03 +0700

    start2

--- js/login.js
+++ js/login.js
@@ -1,42 +1,4 @@
 // ===============================
-<<<<<<< HEAD
-// SUPABASE CONFIG
-// ===============================
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_ANON_KEY";
-
-const { createClient } = supabase;
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
-
-// ===============================
-// LOGIN FUNCTION
-// ===============================
-document.getElementById("loginForm")
-.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const message = document.getElementById("message");
-
-  const { data, error } =
-    await supabaseClient.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-  if (error) {
-    message.innerText = error.message;
-    message.style.color = "red";
-  } else {
-    message.innerText = "Login successful!";
-    message.style.color = "green";
-
-    setTimeout(() => {
-      window.location.href = "dashboard.html";
-    }, 1000);
-  }
-=======
 // Supabase Config
 // ===============================
 const SUPABASE_URL = 'https://vhazgytcfvjhhikiqpwm.supabase.co';
@@ -114,5 +76,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
->>>>>>> 4dfac8f (save before pull)
 });
