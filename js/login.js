490de3a78f2c383b84ed43857dfe2d4ae005e48a
Tonteuy <tonteuy@hotmail.com>
2026-03-10 21:43:35 +0700

    admin css

--- js/login.js
+++ js/login.js
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // 🔥 Redirect ตาม Role
       if (profile.role === "admin") {
-        window.location.href = "admintor.html";
+        window.location.href = "adminDashboard.html";
       } else if (profile.role === "sales") {
         window.location.href = "index.html";
       } else if (profile.role === "manager") {
